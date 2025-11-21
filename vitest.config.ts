9dcd4971a98e1d25b521b5b061cf52b963e73b9d
Ben Houston <neuralsoft@gmail.com>
2025-11-21 15:28:58 -0500

    update to codecov

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     environment: 'node',
     coverage: {
-      reporter: ['text', 'html', 'json', 'json-summary'],
+      reporter: ['text', 'html', 'json', 'json-summary', 'lcov'],
     },
   },
 });
