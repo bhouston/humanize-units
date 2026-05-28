45ab27681a128e2053a6c2b4f52f835407acf6dd
Ben Houston <neuralsoft@gmail.com>
2026-05-28 10:01:09 -0400

    complete upgrade of vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vitest/config';
+import { configDefaults, coverageConfigDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     environment: 'node',
+    include: ['tests/**/*.{test,spec}.?(c|m)[jt]s?(x)'],
+    exclude: [...configDefaults.exclude, '**/coverage/**', '**/dist/**'],
     coverage: {
       reporter: ['text', 'html', 'json', 'json-summary', 'lcov'],
+      exclude: [...coverageConfigDefaults.exclude, '**/dist/**'],
     },
   },
 });
